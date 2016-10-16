e84fa42ee8fa9ff9e9115170a6fdbe67530d3598
Meh Man <Meh Man>
2016-10-16 05:40:13 -0400

    I don't even know at this point.

--- public/js/game.js
+++ public/js/game.js
@@ -4,11 +4,15 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 
 var SorryAlex = 0
-function preload () {
+function preload() {
   this.load.image('earth', 'assets/light_sand.png')
   this.load.image('dude', 'assets/car.png', 64, 64)
   this.load.image('enemy', 'assets/car2.png', 64, 64)
   this.load.tilemap('track1', 'assets/track1.json', null, Phaser.Tilemap.TILED_JSON);
+  this.load.image('tileset', 'assets/tileset.png');
+  this.load.image('finline', 'assets/finline.png');
+  this.load.audio('racestart', 'assets/snd/racestart.wav');
+  this.load.image('go', 'assets/img/go.png');
   this.load.image('tileset', 'assets/tileset.png');
 }
 
@@ -18,83 +22,129 @@ var land
 
 var player
 
+var finline
+
 var enemies
 
 var currentSpeed = 0
 var cursorsrs
 
-function create () {
-socket = io.connect()
+var laps = 0;
+
+function create() {
+  socket = io.connect()
 
   // Resize our game world to be a 2000 x 2000 square
-this.game.world.setBounds(0, 0, 3200, 3200);
+  this.game.world.setBounds(0, 0, 3200, 3200);
 
   // Our tiled scrolling background
-this.map = this.add.tilemap('track1');
-this.map.addTilesetImage('tileset', 'tileset');
+  this.map = this.add.tilemap('track1');
+  this.map.addTilesetImage('tileset', 'tileset');
 
-this.layer = this.map.createLayer('t1');
-this.layer.resizeWorld();
-this.layer.debug = true;
+  this.layer = this.map.createLayer('t1');
+  this.layer.resizeWorld();
+  this.layer.debug = false;
 
-this.map.setCollisionBetween(1 ,271, 272);
-this.map.setCollisionBetween(359, 360);
-var style = { font: "17px Arial", fill: "#192AE3", align: "center" };
-var eStyle = { font: "17px Arial", fill: "#ED0505", align: "center" };
-this.text = this.add.text(0, 0, "You", style);
-this.text1 = this.add.text(0, 0, "Enemy", eStyle);
+  this.map.setCollisionBetween(271, 272);
+  this.map.setCollisionBetween(359, 360);
+
+  this.map.setTileIndexCallback(3073, () => {
+    if (laps == 0) {
+      laps = 1;
+    }
+    if (laps == 4) {
+      laps = 5;
+    }
+    if (laps == 8) {
+      laps = 9;
+    }
+  }, this)
+
+  this.map.setTileIndexCallback(3074, () => {
+    if (laps == 1) {
+      laps = 2;
+    }
+    if (laps == 5) {
+      laps = 6;
+    }
+    if (laps == 9) {
+      laps = 10;
+    }
+  }, this)
+
+  this.map.setTileIndexCallback(3075, () => {
+    if (laps == 2) {
+      laps = 3;
+    }
+    if (laps == 6) {
+      laps = 7;
+    }
+    if (laps == 10) {
+      laps = 11;
+    }
+  }, this)
+
+  var style = { font: "17px Arial", fill: "#192AE3", align: "center" };
+  var eStyle = { font: "17px Arial", fill: "#ED0505", align: "center" };
+  this.text = this.add.text(0, 0, "You", style);
+  this.text1 = this.add.text(0, 0, "Enemy", eStyle);
   // The base of our player
-    var startX = game.rnd.integerInRange(320, 480);
-    var startY = game.rnd.integerInRange(2976, 3072);
-    player = game.add.sprite(startX, startY, 'dude')
-      player.anchor.setTo(0.5, 0.5)
-    game.physics.enable(player, Phaser.Physics.ARCADE);
+  var startX = game.rnd.integerInRange(320, 480);
+  var startY = game.rnd.integerInRange(2976, 3072);
+
+  var finline = this.add.sprite(512, 2944, "finline");
+  this.physics.arcade.enable(finline);
+  finline.body.immovable = true;
+
+  player = game.add.sprite(startX, startY, 'dude')
+  player.anchor.setTo(0.5, 0.5)
+  game.physics.enable(player, Phaser.Physics.ARCADE);
   player.body.maxVelocity.setTo(400, 400)
   player.body.collideWorldBounds = true
 
-this.racestarts = false;
+  this.racestarts = false;
   // This will force it to decelerate and limit its speed
   // player.body.drag.setTo(200, 200)
 
   this.time.events.add(Phaser.Timer.SECOND * 1, () => {
-            this.racestart = this.add.audio('racestart');
-            this.racestart.play();
-            this.three = this.add.text(395, 250, "3");
-            this.three.fill = "#ffffff";
-            this.three.fixedToCamera = true;
-    });
+    this.racestart = this.add.audio('racestart');
+    this.racestart.play();
+    this.three = this.add.text(395, 250, "3");
+    this.three.fill = "#ffffff";
+    this.three.fixedToCamera = true;
+  });
 
-    this.time.events.add(Phaser.Timer.SECOND * 2, () => {
-            this.three.alpha = 0;
-            this.two = this.add.text(395, 250, "2");
-            this.two.fill = "#ffffff";
-            this.two.fixedToCamera = true;
-            console.log(this.racestarts);
+  this.time.events.add(Phaser.Timer.SECOND * 2, () => {
+    this.three.alpha = 0;
+    this.two = this.add.text(395, 250, "2");
+    this.two.fill = "#ffffff";
+    this.two.fixedToCamera = true;
+    console.log(this.racestarts);
 
-    });
+  });
 
-    this.time.events.add(Phaser.Timer.SECOND * 3, () => {
-            this.two.alpha = 0;
-            this.one = this.add.text(395, 250, "1");
-            this.one.fill = "#ffffff";
-            this.one.fixedToCamera = true;
- console.log(this.racestarts);
+  this.time.events.add(Phaser.Timer.SECOND * 3, () => {
+    this.two.alpha = 0;
+    this.one = this.add.text(395, 250, "1");
+    this.one.fill = "#ffffff";
+    this.one.fixedToCamera = true;
+    console.log(this.racestarts);
 
-    });
+  });
 
-    this.time.events.add(Phaser.Timer.SECOND * 4, () => {
-            this.one.alpha = 0;
-            this.go = this.add.text(395, 250, "Go!");
-            this.go.fill = "#ffffff";
-            this.go.fixedToCamera = true;
-            this.racestarts = true;
-console.log(this.racestarts);
-    });
-    
-    this.time.events.add(Phaser.Timer.SECOND * 5, () => {
-            this.go.alpha = 0;
+  this.time.events.add(Phaser.Timer.SECOND * 4, () => {
+    this.one.alpha = 0;
+    this.go = this.add.text(395, 250, "Go!");
+    this.go.fill = "#ffffff";
+    this.go.fixedToCamera = true;
+    this.racestarts = true;
+    console.log(this.racestarts);
+  });
 
-    });
+  this.time.events.add(Phaser.Timer.SECOND * 5, () => {
+    this.go.alpha = 0;
+
+  });
 
   // Create some baddies to waste :)
   enemies = []
@@ -129,7 +179,7 @@ var setEventHandlers = function () {
 }
 
 // Socket connected
-function onSocketConnected () {
+function onSocketConnected() {
   console.log('Connected to socket server')
 
   // Reset enemies on reconnect
@@ -143,12 +193,12 @@ function onSocketConnected () {
 }
 
 // Socket disconnected
-function onSocketDisconnect () {
+function onSocketDisconnect() {
   console.log('Disconnected from socket server')
 }
 
 // New player
-function onNewPlayer (data) {
+function onNewPlayer(data) {
   console.log('New player connected:', data.id)
 
   // Avoid possible duplicate players
@@ -163,7 +213,7 @@ function onNewPlayer (data) {
 }
 
 // Move player
-function onMovePlayer (data) {
+function onMovePlayer(data) {
   var movePlayer = playerById(data.id)
 
   // Player not found
@@ -179,7 +229,7 @@ function onMovePlayer (data) {
 }
 
 // Remove player
-function onRemovePlayer (data) {
+function onRemovePlayer(data) {
   var removePlayer = playerById(data.id)
 
   // Player not found
@@ -194,59 +244,58 @@ function onRemovePlayer (data) {
   enemies.splice(enemies.indexOf(removePlayer), 1)
 }
 
-function update () {
-if(this.racestarts == true){
-game.physics.arcade.collide(player, this.layer);
+function update() {
+  console.log(laps);
+  if (this.racestarts == true) {
+    game.physics.arcade.collide(player, this.layer);
+    if (laps => 3) {
+      this.physics.arcade.collide(player, finline, lapCount, null, this);
+    }
 
+    this.text.x = player.x - 15
+    this.text.y = player.y - 40
 
-this.text.x = player.x -15
-this.text.y = player.y -40
+    if (SorryAlex = 5) {
+      if (cursors.left.isDown) {
+        player.angle -= 4
+      } else if (cursors.right.isDown) {
+        player.angle += 4
+      }
 
-if (SorryAlex = 5){
-  if (cursors.left.isDown) {
-    player.angle -= 4
-  } else if (cursors.right.isDown) {
-    player.angle += 4
-  }
+      if (cursors.up.isDown) {
+        // The speed we'll travel at
+        currentSpeed = 300
+      } else {
+        if (currentSpeed > 0) {
+          currentSpeed -= 4
+        }
+      }
 
-  if (cursors.up.isDown) {
-    // The speed we'll travel at
-    currentSpeed = 300
-  } else {
-    if (currentSpeed > 0) {
-      currentSpeed -= 4
+      game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity)
+
+      if (game.input.activePointer.isDown) {
+        if (game.physics.arcade.distanceToPointer(player) >= 10) {
+          currentSpeed = 300
+
+          player.rotation = game.physics.arcade.angleToPointer(player)
+        }
+      }
+
+      socket.emit('move player', { x: player.x, y: player.y, angle: player.angle })
     }
   }
-
-  game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity)
-
-
-
-
-  if (game.input.activePointer.isDown) {
-    if (game.physics.arcade.distanceToPointer(player) >= 10) {
-      currentSpeed = 300
-
-      player.rotation = game.physics.arcade.angleToPointer(player)
-    }
-  }
-
-  socket.emit('move player', { x: player.x, y: player.y, angle: player.angle })
- }
- }
 }
 
 
-function ColsCheck ()
-{
+function ColsCheck() {
 }
 
-function render () {
+function render() {
 
 }
 
 // Find player by ID
-function playerById (id) {
+function playerById(id) {
   for (var i = 0; i < enemies.length; i++) {
     if (enemies[i].player.name === id) {
       return enemies[i]
@@ -254,4 +303,19 @@ function playerById (id) {
   }
 
   return false
+}
+
+function lapCount() {
+  player.x = player.x - 65
+  if (laps == 3) {
+    console.log("Lap 2");
+    laps = 4;
+  }
+  if (laps == 7) {
+    console.log("Lap 3");
+    laps = 8;
+  }
+  if (laps == 11) {
+    console.log("You win!");
+  }
 }
