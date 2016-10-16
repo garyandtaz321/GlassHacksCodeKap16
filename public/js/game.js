5ccf33564d59b6aca538972eb87865a04b64ee02
kkuhn <kkuhn0000@hotmail.com>
2016-10-15 22:46:38 -0700

    asdasd

---
+++ public/js/game.js
@@ -0,0 +1,220 @@
+/* global Phaser RemotePlayer io */
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render })
+
+function preload () {
+  game.load.image('earth', 'assets/light_sand.png')
+  game.load.image('dude', 'assets/car.png', 64, 64)
+  game.load.image('enemy', 'assets/car2.png', 64, 64)
+      this.load.tilemap('track1', 'assets/track1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('tileset', 'assets/tileset.png');
+}
+
+var socket // Socket connection
+
+var land
+
+var player
+
+var enemies
+
+var currentSpeed = 0
+var cursorsrs
+
+function create () {
+  socket = io.connect()
+
+  // Resize our game world to be a 2000 x 2000 square
+    this.game.world.setBounds(0, 0, 3200, 3200);
+
+  // Our tiled scrolling background
+    this.map = this.add.tilemap('track1');
+    this.map.addTilesetImage('tileset', 'tileset');
+
+    this.layer = this.map.createLayer('t1');
+    this.layer.resizeWorld();
+    this.layer.debug = true;
+
+    this.map.setCollisionBetween(271, 272);
+    this.map.setCollisionBetween(359, 360);
+  var style = { font: "17px Arial", fill: "#192AE3", align: "center" };
+    var eStyle = { font: "17px Arial", fill: "#ED0505", align: "center" };
+this.text = this.add.text(0, 0, "You", style);
+this.text1 = this.add.text(0, 0, "Enemy", eStyle);
+  // The base of our player
+  var startX = Math.round(Math.random() * (1000) - 500)
+  var startY = Math.round(Math.random() * (1000) - 500)
+  player = game.add.sprite(startX, startY, 'dude')
+
+  player.anchor.setTo(0.5, 0.5)
+
+  // This will force it to decelerate and limit its speed
+  // player.body.drag.setTo(200, 200)
+  game.physics.enable(player, Phaser.Physics.ARCADE);
+  player.body.maxVelocity.setTo(400, 400)
+  player.body.collideWorldBounds = true
+
+  // Create some baddies to waste :)
+  enemies = []
+
+  player.bringToTop()
+
+  game.camera.follow(player)
+  game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300)
+  game.camera.focusOnXY(0, 0)
+
+  cursors = game.input.keyboard.createCursorKeys()
+
+  // Start listening for events
+  setEventHandlers()
+}
+
+var setEventHandlers = function () {
+  // Socket connection successful
+  socket.on('connect', onSocketConnected)
+
+  // Socket disconnection
+  socket.on('disconnect', onSocketDisconnect)
+
+  // New player message received
+  socket.on('new player', onNewPlayer)
+
+  // Player move message received
+  socket.on('move player', onMovePlayer)
+
+  // Player removed message received
+  socket.on('remove player', onRemovePlayer)
+}
+
+// Socket connected
+function onSocketConnected () {
+  console.log('Connected to socket server')
+
+  // Reset enemies on reconnect
+  enemies.forEach(function (enemy) {
+    enemy.player.kill()
+  })
+  enemies = []
+
+  // Send local player data to the game server
+  socket.emit('new player', { x: player.x, y: player.y, angle: player.angle })
+}
+
+// Socket disconnected
+function onSocketDisconnect () {
+  console.log('Disconnected from socket server')
+}
+
+// New player
+function onNewPlayer (data) {
+  console.log('New player connected:', data.id)
+
+  // Avoid possible duplicate players
+  var duplicate = playerById(data.id)
+  if (duplicate) {
+    console.log('Duplicate player!')
+    return
+  }
+
+  // Add new player to the remote players array
+  enemies.push(new RemotePlayer(data.id, game, player, data.x, data.y, data.angle))
+}
+
+// Move player
+function onMovePlayer (data) {
+  var movePlayer = playerById(data.id)
+
+  // Player not found
+  if (!movePlayer) {
+    console.log('Player not found: ', data.id)
+    return
+  }
+
+  // Update player position
+  movePlayer.player.x = data.x
+  movePlayer.player.y = data.y
+  movePlayer.player.angle = data.angle
+}
+
+// Remove player
+function onRemovePlayer (data) {
+  var removePlayer = playerById(data.id)
+
+  // Player not found
+  if (!removePlayer) {
+    console.log('Player not found: ', data.id)
+    return
+  }
+
+  removePlayer.player.kill()
+
+  // Remove player from array
+  enemies.splice(enemies.indexOf(removePlayer), 1)
+}
+
+function update () {
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].alive) {
+      enemies[i].update()
+      game.physics.arcade.collide(player, enemies[i].player, this.ColsCheck, null , this )
+
+this.text.x = player.x -15
+this.text.y = player.y -40
+
+
+
+
+
+    }
+  }
+
+  if (cursors.left.isDown) {
+    player.angle -= 4
+  } else if (cursors.right.isDown) {
+    player.angle += 4
+  }
+
+  if (cursors.up.isDown) {
+    // The speed we'll travel at
+    currentSpeed = 300
+  } else {
+    if (currentSpeed > 0) {
+      currentSpeed -= 4
+    }
+  }
+
+  game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity)
+
+
+
+
+  if (game.input.activePointer.isDown) {
+    if (game.physics.arcade.distanceToPointer(player) >= 10) {
+      currentSpeed = 300
+
+      player.rotation = game.physics.arcade.angleToPointer(player)
+    }
+  }
+
+  socket.emit('move player', { x: player.x, y: player.y, angle: player.angle })
+}
+
+
+function ColsCheck ()
+{
+}
+
+function render () {
+
+}
+
+// Find player by ID
+function playerById (id) {
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].player.name === id) {
+      return enemies[i]
+    }
+  }
+
+  return false
+}
